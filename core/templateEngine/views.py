701270023fca44ccb02f29f15f66f0c0edf1960d
Zlaam <mrzalaam@gmail.com>
2023-12-01 12:44:50 +0500

    lastStable commit

--- core/templateEngine/views.py
+++ core/templateEngine/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render
 
 
 def test_helloworld(request):
-    return render(request, "index.html")
+    titles = ["S.No", "Name", "Age", "Role"]
+    people = [
+        {"name": "Zlaam", "role": "FullstackDev", "age": 23},
+        {"name": "Siraj", "role": "Frontend-Devloper", "age": 20},
+        {"name": "Khan", "role": "Backend-Developer", "age": 21},
+    ]
+    return render(request, "index.html", context={"peoples": people, "titles": titles})
