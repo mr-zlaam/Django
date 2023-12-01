141469c6aa626bfa22d6421bd3ddb423c72cee81
Zlaam <mrzalaam@gmail.com>
2023-12-01 18:03:14 +0500

    last stable commit

--- core/core/settings.py
+++ core/core/settings.py
@@ -116,9 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+
+import os
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_DIR = [os.path.join(BASE_DIR, "public/static")]
+MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
