6b830645acd5ab6f1428df514f2c77ec3fc161ae
Zlaam <mrzalaam@gmail.com>
2023-12-01 16:51:02 +0500

    last stable commit

--- core/vege/views.py
+++ core/vege/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import *
 
 
 # Create your views here.
-def recipies(require):
-    return render(require, "home.html")
+def recipies(request):
+    if request.method == "POST":
+        data = request.POST
+        racipe_name = data.get("racipe_name")
+        racipe_description = data.get("racipe_description")
+        racipe_image = request.FILES.get("racipe_image")
+        Recipe.objects.create(
+            racipe_name=racipe_name,
+            racipe_description=racipe_description,
+            racipe_image=racipe_image,
+        )
+        return redirect("/")
+    return render(request, "home.html")
