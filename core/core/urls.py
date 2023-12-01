62e8ef80bab65f248ca3f5ce0f4b8de6e59f1601
Zlaam <mrzalaam@gmail.com>
2023-12-01 11:51:43 +0500

    last stable commit

--- core/core/urls.py
+++ core/core/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from templateEngine.views import *
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", test_helloworld, name="home")]
