53ce3d81c183b2dcd58e2e8242dc27dadc1e9c21
Zlaam <mrzalaam@gmail.com>
2023-12-03 10:41:33 +0500

    last stable commit

--- core/vege/views.py
+++ core/vege/views.py
@@ -22,12 +22,26 @@ def recipies(request):
 
 
 def delete_recipe(request, id):
-    queryset = Recipe.objects.filter(id=id)
+    queryset = Recipe.objects.get(id=id)
     queryset.delete()
     return redirect("/")
 
 
 def update_recipe(request, id):
-    queryset = Recipe.objects.filter(id=id)
-    queryset.delete()
-    return redirect("/")
+    queryset = Recipe.objects.get(id=id)
+    if request.method == "POST":
+        data = request.POST
+        racipe_name = data.get("racipe_name")
+        racipe_description = data.get("racipe_description")
+        racipe_image = request.FILES.get("racipe_image")
+
+        queryset.racipe_name = racipe_name
+        queryset.racipe_description = racipe_description
+        if racipe_image:
+            queryset.racipe_image = racipe_image
+
+        queryset.save()
+        return redirect("/")
+
+    context = {"recipe": queryset}
+    return render(request, "update_recipe.html", context)
